b8b537fefef3c90f26e2e7a21f2b481cbd6f5b00
Rita DeBraal <ladyautodr@gmail.com>
2019-08-29 00:51:41 -0500

    Exercise 4.3

---
+++ week-4/status-codes/debraal-exercise-4.3.js
@@ -0,0 +1,13 @@
+/*
+======================================
+Title: Exercise 4.3 Status Codes
+Author: Rita DeBraal
+Date: 22 August 2019
+Description: Demonstrate Status Codes
+======================================
+*/
+
+// Import statement from header file
+const header = require('../../debraal-header.js')
+// Print statement with firstName, lastName, and assignment parameters
+console.log(header.display('Rita', 'DeBraal', 'Exercise 4.2'))
