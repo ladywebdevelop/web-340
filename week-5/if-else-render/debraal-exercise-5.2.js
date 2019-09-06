feb8e85b00fd95b00c54ee4ba79a84e605b8d447
Rita DeBraal <ladyautodr@gmail.com>
2019-09-06 04:09:28 -0500

    Exercise 5.2

--- week-5/if-else-render/debraal-exercise-5.2.js
+++ week-5/if-else-render/debraal-exercise-5.2.js
@@ -10,7 +10,7 @@ Description: Demonstrate EJS Templates
 // Import statement from header file
 const header = require('../../debraal-header.js')
 // Print statement with firstName, lastName, and assignment parameters
-console.log(header.display('Rita', 'DeBraal', 'Exercise 4.3'))
+console.log(header.display('Rita', 'DeBraal', 'Exercise 5.2'))
 
 var express = require("express");
 var http = require("http");
