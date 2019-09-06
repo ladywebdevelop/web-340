c0908d8931904496505464eed3a487a651f26acf
Rita DeBraal <ladyautodr@gmail.com>
2019-09-06 04:10:28 -0500

    Exercise 5.3

---
+++ week-5/pug-template/debraal-exercise-5.3.js
@@ -0,0 +1,13 @@
+/*
+======================================
+Title: Exercise 5.3 Pug Template
+Author: Rita DeBraal
+Date: 6 September 2019
+Description: Demonstrate PUG Templates
+======================================
+*/
+
+// Import statement from header file
+const header = require('../../debraal-header.js')
+// Print statement with firstName, lastName, and assignment parameters
+console.log(header.display('Rita', 'DeBraal', 'Exercise 5.3'))
