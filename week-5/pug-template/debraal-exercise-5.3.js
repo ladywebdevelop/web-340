4cd6396709d0de43bcd2d1460d192062db6f5630
Rita DeBraal <ladyautodr@gmail.com>
2019-09-06 04:20:39 -0500

    Exercise 5.3 Server

--- week-5/pug-template/debraal-exercise-5.3.js
+++ week-5/pug-template/debraal-exercise-5.3.js
@@ -11,3 +11,25 @@ Description: Demonstrate PUG Templates
 const header = require('../../debraal-header.js')
 // Print statement with firstName, lastName, and assignment parameters
 console.log(header.display('Rita', 'DeBraal', 'Exercise 5.3'))
+
+var express = require("express");
+var http = require("http");
+var path = require("path");
+var logger = require("morgan");
+
+var app = express();
+
+app.set("views", path.resolve(__dirname, "views"));
+
+app.set("view engine", "pug");
+
+app.get("/", function(req, res) {
+  res.render("index", {
+    message:"I\'ve a soft spot for Klinger. He looks a little like" +
+            "my son, and he dresses a lot like my wife."
+  });
+});
+
+http.createServer(app).listen(8080, function() {
+  console.log("Application started on port 8080!");
+});
