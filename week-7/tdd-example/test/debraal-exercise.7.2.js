3bba201b45f558a9d80b47254f085c0a5a9a5012
Rita DeBraal <ladyautodr@gmail.com>
2019-09-18 01:02:17 -0500

    Exercise 7.2

---
+++ week-7/tdd-example/test/debraal-exercise.7.2.js
@@ -0,0 +1,7 @@
+var assert = require("assert");
+
+describe("String#split", function() {
+  it("should return an array of fruits", function() {
+    assert(Array.isArray('Apple,Orange,Mango'.split(',')));
+  });
+});
