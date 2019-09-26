3c4b17e6add78044df6d59b986a7ff926672d1a1
Rita DeBraal <ladyautodr@gmail.com>
2019-09-26 05:33:34 -0500

    Week-8

---
+++ week-8/xss-prevention/debraal-exercise-8.2.js
@@ -0,0 +1,9 @@
+/*
+============================================
+; Title:  Cross-site Scripting
+; Author: Professor Krasso
+; Date:   26 September 2019
+; Description: Demonstrates how to configure an application for
+;              XSS threats.
+;===========================================
+*/
