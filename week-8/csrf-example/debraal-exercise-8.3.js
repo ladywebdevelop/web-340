e0ad473ddf4eb018e8582581664f7f8f7262e7ef
Rita DeBraal <ladyautodr@gmail.com>
2019-09-26 16:40:06 -0500

    Exercise 8.3

---
+++ week-8/csrf-example/debraal-exercise-8.3.js
@@ -0,0 +1,62 @@
+/*
+============================================
+; Title:  Cross-site Request Forgery
+; Author: Rita DeBraal
+; Date:   26 September 2019
+; Description: Demonstrates how to configure an application for
+;              CSRF protection
+;===========================================
+*/
+
+// Require Statements
+var express = require("express");
+var http = require("http");
+var path = require("path");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var csrf = require("csurf");
+
+// csrf protection setup
+var csrfProtection = csrf({ cookie: true});
+
+// express application initialization
+var app = express();
+
+// use statements
+app.use(logger("short"));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.use(cookieParser());
+app.use(csrfProtection);
+app.use(function(req, res, next) {
+  var token = request.csrfToken();
+  res.cookie("XSRF-TOKEN", token);
+  res.locals.csrfToken = token;
+  next();
+});
+
+// Set statements
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Routing
+app.get("/", function(request, response) {
+  response.render("index", {
+    message: "New Fruit Entry Page"
+  });
+});
+
+app.post("/process", function(request, response) {
+  console.log(request.body.txtName);
+  response.redirect("/");
+});
+
+// Create and confirm Node server
+http.createServer(app).listen(8080, function() {
+  console.log("Application started on port 8080!");
+});
