91752affe40e0e18f95201cd85b81ccb6bb1aca2
Rita DeBraal <ladyautodr@gmail.com>
2019-09-26 05:47:08 -0500

    Exercise 8.2

--- week-8/xss-prevention/debraal-exercise-8.2.js
+++ week-8/xss-prevention/debraal-exercise-8.2.js
@@ -7,3 +7,33 @@
 ;              XSS threats.
 ;===========================================
 */
+
+// Require Statements
+var express = require("express");
+var http = require("http");
+var logger = require("morgan");
+var helmet = require("helmet");
+var path = require("path");
+
+// Express Initialize
+var app = express();
+
+// Use statements
+app.use(logger("short"));
+app.use(helmet.xssFilter());
+
+// Set statements
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Calls http
+app.get("/", function(req, res) {
+  res.render("index", {
+    message: "XSS Prevention Example"
+  });
+});
+
+// Create & confirm port starting
+http.createServer(app).listen(8080, function() {
+  console.log("Application started on port 8080!");
+})
